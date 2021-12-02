754ad9025c9df210d1e4b15f37d85e55d0f91c61
Stilan <40999153+Stilan@users.noreply.github.com>
2021-12-02 18:39:49 +0700

    2. Scriplet. Динамическая таблица. [#276520 #235068]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -13,9 +13,12 @@ public class Store {
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
     private Store() {
-        posts.put(1, new Post(1, "Junior Java Job",  "Тех.стек: Java, JavaScript, Spring, Hibernate, ReactJS.",  "1.12.2021"));
-        posts.put(2, new Post(2, "Middle Java Job", "Наш основной стек Java 8, Spring Boot, PostgreSQL, JUnit, Maven, Gradle, Git, Docker и Kubernetes ", "30.11.2021"));
-        posts.put(3, new Post(3, "Senior Java Job", "Опыт с Java Core, Spring Framework, Hibernate", "29.11.2021"));
+        posts.put(1, new Post(1, "Junior Java Job",
+                "Тех.стек: Java, JavaScript, Spring, Hibernate, ReactJS.",  "1.12.2021"));
+        posts.put(2, new Post(2, "Middle Java Job",
+                "Наш основной стек Java 8, Spring Boot, PostgreSQL, JUnit, Maven, Gradle, Git, Docker и Kubernetes ", "30.11.2021"));
+        posts.put(3, new Post(3, "Senior Java Job",
+                "Опыт с Java Core, Spring Framework, Hibernate", "29.11.2021"));
     }
 
     public static Store instOf() {
