675ea267aec20d4edae3bedc20612226c9e69479
Stilan <40999153+Stilan@users.noreply.github.com>
2021-12-03 14:35:15 +0700

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
@@ -12,6 +13,8 @@ public class Store {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job",
                 "Тех.стек: Java, JavaScript, Spring, Hibernate, ReactJS.",  "1.12.2021"));
@@ -19,13 +22,21 @@ public class Store {
                 "Наш основной стек Java 8, Spring Boot, PostgreSQL, JUnit, Maven, Gradle, Git, Docker и Kubernetes ", "30.11.2021"));
         posts.put(3, new Post(3, "Senior Java Job",
                 "Опыт с Java Core, Spring Framework, Hibernate", "29.11.2021"));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
+    }
+
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -29,7 +29,7 @@
             </tr>
             </thead>
             <tbody>
-            <% for (Post post : Store.instOf().findAll()) { %>
+            <% for (Post post : Store.instOf().findAllPosts()) { %>
             <tr>
                 <td><%=post.getName()%>
                 <td><%=post.getDescription()%> </td>
